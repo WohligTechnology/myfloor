4d9d1fb9fae7851c3efaa2591f9e07118ecff03d
mahesh maurya <mahesh@wohlig.com>
2016-10-26 11:49:14 +0530

    changes in slider

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,8 +4,12 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('HomeCtrl', function($scope) {
-
+.controller('HomeCtrl', function($scope,$ionicSlideBoxDelegate) {
+$scope.slider=[
+  'img/4.jpg',
+  'img/2.jpg',
+  'img/3.jpg'
+]
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
