3ce99c4f555433ba42cc6c07a03e16f63f5ca0f2
NileshWangad <ruchira.padave29@gmail.com>
2017-06-17 13:39:38 +0530

    thanksmodal,controller changes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', ['starter.services', 'ionic', 'tabSlideBox', 'ngCordova'])
 
-.controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
+  .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
 
   })
   .controller('ProductCategoryCtrl', function ($scope, $ionicModal, $timeout) {
@@ -107,51 +107,51 @@ angular.module('starter.controllers', ['starter.services', 'ionic', 'tabSlideBox
     }
   })
 
-.controller('HomeCtrl', function ($scope, $ionicSlideBoxDelegate, MyServices) {
-  $scope.homeSlider = {};
-  $scope.homeSlider.data = [];
-  $scope.homeSlider.currentPage = 0;
+  .controller('HomeCtrl', function ($scope, $ionicSlideBoxDelegate, MyServices) {
+    $scope.homeSlider = {};
+    $scope.homeSlider.data = [];
+    $scope.homeSlider.currentPage = 0;
 
-  MyServices.getAllHomeData({}, function (data) {
-    if (data.value) {
-      $scope.homeSlider.data = data.data.HomeSlider;
-      $scope.landingBanner = data.data.LandingBanner;
-      // console.log($scope.homeSlider, $scope.landingBanner);
-    } else {
+    MyServices.getAllHomeData({}, function (data) {
+      if (data.value) {
+        $scope.homeSlider.data = data.data.HomeSlider;
+        $scope.landingBanner = data.data.LandingBanner;
+        // console.log($scope.homeSlider, $scope.landingBanner);
+      } else {
 
-    }
-  });
-
-  $scope.setupSlider = function () {
-
-    //some options to pass to our slider
-    $scope.homeSlider.sliderOptions = {
-      initialSlide: 0,
-      direction: 'horizontal', //or vertical
-      speed: 300,
-      pagination: false,
-      effect: 'fade',
-    };
-
-    //create delegate reference to link with slider
-    $scope.homeSlider.sliderDelegate = null;
-
-    //watch our sliderDelegate reference, and use it when it becomes available
-    $scope.$watch('homeSlider.sliderDelegate', function (newVal, oldVal) {
-      if (newVal != null) {
-        $scope.homeSlider.sliderDelegate.on('slideChangeEnd', function () {
-          $scope.homeSlider.currentPage = $scope.homeSlider.sliderDelegate.activeIndex;
-          //use $scope.$apply() to refresh any content external to the slider
-          $scope.$apply();
-        });
       }
     });
-  };
 
-  $scope.setupSlider();
+    $scope.setupSlider = function () {
+
+      //some options to pass to our slider
+      $scope.homeSlider.sliderOptions = {
+        initialSlide: 0,
+        direction: 'horizontal', //or vertical
+        speed: 300,
+        pagination: false,
+        effect: 'fade',
+      };
+
+      //create delegate reference to link with slider
+      $scope.homeSlider.sliderDelegate = null;
+
+      //watch our sliderDelegate reference, and use it when it becomes available
+      $scope.$watch('homeSlider.sliderDelegate', function (newVal, oldVal) {
+        if (newVal != null) {
+          $scope.homeSlider.sliderDelegate.on('slideChangeEnd', function () {
+            $scope.homeSlider.currentPage = $scope.homeSlider.sliderDelegate.activeIndex;
+            //use $scope.$apply() to refresh any content external to the slider
+            $scope.$apply();
+          });
+        }
+      });
+    };
+
+    $scope.setupSlider();
 
 
-  MyServices.getAllCollection({}, function (data) {
+    MyServices.getAllCollection({}, function (data) {
       if (data.value) {
         $scope.getcollection = data.data;
         // console.log($scope.homeSlider, $scope.landingBanner);
@@ -164,107 +164,107 @@ angular.module('starter.controllers', ['starter.services', 'ionic', 'tabSlideBox
     //   'img/23.jpg',
     //   'img/123.jpg'
     // ];
-  $scope.allproduct = [{
-        title1: "Chalet",
+    $scope.allproduct = [{
+      title1: "Chalet",
 
-        image: "img/123.jpg"
-      }, {
-        title1: "Vintage Loft",
-        image: "img/124.jpg"
-      }, {
-        title1: "Saltbox",
-        image: "img/12.jpg"
-      }, {
-        title1: "Gallery",
-        image: "img/12.jpg"
-      }, {
-        title1: "Brick & Board",
-        image: "img/12.jpg"
-      }, {
-        title1: "Storehouse Plank",
-        image: "img/12.jpg"
-      }, {
-        title1: "Eighteen Seventy-Five",
-        image: "img/23.jpg"
+      image: "img/123.jpg"
+    }, {
+      title1: "Vintage Loft",
+      image: "img/124.jpg"
+    }, {
+      title1: "Saltbox",
+      image: "img/12.jpg"
+    }, {
+      title1: "Gallery",
+      image: "img/12.jpg"
+    }, {
+      title1: "Brick & Board",
+      image: "img/12.jpg"
+    }, {
+      title1: "Storehouse Plank",
+      image: "img/12.jpg"
+    }, {
+      title1: "Eighteen Seventy-Five",
+      image: "img/23.jpg"
 
-      }, {
-        title1: "Ponderosa",
-        image: "img/23.jpg"
-      }
+    }, {
+      title1: "Ponderosa",
+      image: "img/23.jpg"
+    }
 
     ]
     //mission
-  MyServices.getAboutUs({
-    _id: "593fd4f92bae30327e260222"
-  }, function (response) {
-    if (response.value) {
-      $scope.mission = response.data.content;
-      console.log(" $scope.mission", $scope.mission);
-    } else {
+    MyServices.getAboutUs({
+      _id: "593fd4f92bae30327e260222"
+    }, function (response) {
+      if (response.value) {
+        $scope.mission = response.data.content;
+        console.log(" $scope.mission", $scope.mission);
+      } else {
 
+      }
+    });
+
+    //vission
+    MyServices.getAboutUs({
+      _id: "593fd4fe2bae30327e260223"
+    }, function (response) {
+      if (response.value) {
+        $scope.vission = response.data.content;
+        console.log(" $scope.vission", $scope.vission);
+      } else {
+
+      }
+    });
+  })
+
+  .controller('SearchCtrl', function ($scope, $state, $ionicSlideBoxDelegate, MyServices) {
+
+    $scope.searchText = "";
+    // $scope.productArray = [];
+    // $scope.collectionArray = [];
+    $scope.search = function (value) {
+      console.log("value", value);
+      $scope.isText = true;
+      $scope.productArray = [];
+      $scope.collectionArray = [];
+      if (value.searchText != "") {
+        MyServices.searchData({
+          searchText: value
+        }, function (data) {
+          if (data.value) {
+            $scope.productArray = data.data.product;
+            $scope.collectionArray = data.data.collection;
+          } else {
+            $scope.productArray = [];
+            $scope.collectionArray = [];
+          }
+        })
+      }
     }
-  });
 
-  //vission
-  MyServices.getAboutUs({
-    _id: "593fd4fe2bae30327e260223"
-  }, function (response) {
-    if (response.value) {
-      $scope.vission = response.data.content;
-      console.log(" $scope.vission", $scope.vission);
-    } else {
+    //To search product
+    $scope.getProduct = function (value) {
+      var product = $scope.productArray[value];
+      $state.go('app.collection-detail', {
+        productId: product._id
+      })
+    };
 
-    }
-  });
-})
-
-.controller('SearchCtrl', function ($scope, $state, $ionicSlideBoxDelegate, MyServices) {
-
-  $scope.searchText = "";
-  // $scope.productArray = [];
-  // $scope.collectionArray = [];
-  $scope.search = function (value) {
-    console.log("value", value);
-    $scope.isText = true;
-    $scope.productArray = [];
-    $scope.collectionArray = [];
-    if (value.searchText != "") {
-      MyServices.searchData({
-        searchText: value
-      }, function (data) {
-        if (data.value) {
-          $scope.productArray = data.data.product;
-          $scope.collectionArray = data.data.collection;
-        } else {
-          $scope.productArray = [];
-          $scope.collectionArray = [];
-        }
+    //To search Collection 
+    $scope.getCollection = function (value) {
+      var collection = $scope.collectionArray[value];
+      $state.go('app.product-detail', {
+        title: collection.name,
+        id: collection._id
       })
     }
-  }
-
-  //To search product
-  $scope.getProduct = function (value) {
-    var product = $scope.productArray[value];
-    $state.go('app.collection-detail', {
-      productId: product._id
-    })
-  };
-
-  //To search Collection 
-  $scope.getCollection = function (value) {
-    var collection = $scope.collectionArray[value];
-    $state.go('app.product-detail', {
-      title: collection.name,
-      id: collection._id
-    })
-  }
-})
+  })
 
 
 
 
-.controller('AboutUsCtrl', function ($scope, $ionicSlideBoxDelegate, MyServices) {
+  .controller('AboutUsCtrl', function ($scope, $ionicSlideBoxDelegate, MyServices) {
 
     //API call to get about us info
     MyServices.getAboutUs({
@@ -367,17 +367,25 @@ angular.module('starter.controllers', ['starter.services', 'ionic', 'tabSlideBox
     $scope.submitForm = function (data) {
       console.log(data);
       MyServices.saveConatct(data, function (response) {
-          if (response.value) {
-            $state.reload();
-            $scope.formData = {};
-            console.log("response.data", response.data);
-            //   toastr.success("We will get back to you shortly.", "We have your query!");
-          }
+
+        if (response.value) {
+          //   $ionicModal.fromTemplateUrl('templates/videomodal.html', {
+          //   scope: $scope,
+          //   animation: 'slide-in-up'
+          // }).then(function (modal) {
+          //   $scope.modal = modal;
+
+          // });
+          $state.reload();
           $scope.formData = {};
-        })
-        // $scope.formSubmitted = true;
+          console.log("response.data", response.data);
+          //   toastr.success("We will get back to you shortly.", "We have your query!");
+        }
+        $scope.formData = {};
+      })
+      // $scope.formSubmitted = true;
     }
 
   })
 
-.controller('PlaylistCtrl', function ($scope, $stateParams) {});
+  .controller('PlaylistCtrl', function ($scope, $stateParams) { });
